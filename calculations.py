9235f8c8a93ae304fde7ce6132bc5e0cd9756150
William <whou125@gmail.com>
2018-03-19 00:35:16 +0000

    Modify function to include average switch ownership

--- calculations.py
+++ calculations.py
@@ -7,16 +7,24 @@ from utils import *
 TEAM = 2521
 TEAM = "frc" + str(TEAM)
 
-def average_teleop_scale_ownership(team_key):
+def average_teleop_ownership(team_key, structure):
     matches = match_request_handler(team_key)
+    structure = structure.title()
     total = []
     for match in matches:
         if match_exists(matches,match["key"],team_key):
             # print(match["score_breakdown"][alliance_color(match["key"])]["teleopSwitchOwnershipSec"])
-            total.append(match["score_breakdown"][alliance_color(matches,match["key"],team_key)]["teleopScaleOwnershipSec"])
+            try:
+                total.append(match["score_breakdown"][alliance_color(matches,match["key"],team_key)]["teleop{}OwnershipSec".format(structure)])
+            except KeyError:
+                print("That's not a valid game structure!")
+                break
     return round(sum(total)/len(total),2)
     
     
+    
+    
+    
 if __name__ == "__main__":    
-    print(average_teleop_scale_ownership(TEAM))
+    print(average_teleop_ownership(TEAM, "scale"))
     
--- evaluations.py
+++ evaluations.py
@@ -8,8 +8,7 @@ event = event_request_handler(EVENT)
 
 # Average scale ownership for robot's alliance
 for team in event:
-    print(team["nickname"] + " : " + str(average_teleop_scale_ownership("frc" + str(team["team_number"]))) + "s")
+    print(team["nickname"] + " : " + str(average_teleop_ownership("frc" + str(team["team_number"]), "scale")) + "s")
     # print((team["nickname"],str(average_teleop_scale_ownership("frc" + str(team["team_number"])))))
-    
     
     
