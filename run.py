0a6f24618b2a74d0fc8be90fd4f0fd782f2b866a
William <whou125@gmail.com>
2018-03-30 05:47:57 +0000

    make things easier to read

--- run.py
+++ run.py
@@ -1,15 +1,26 @@
 from functions import *
 
-TEAM = 2521
+TEAM = 5450
 TEAM = "frc" + str(TEAM)
 
 # 2018orwil 2018orlak
 EVENT = "2018orlak"
     
-if __name__ == "__main__":    
-    print(average_teleop_ownership(TEAM, "scale", True))
-    for key, value in average_teleop_ownership_eventleaderboard(EVENT, "scale"):
-        print("{team} : {time}s".format(team=key,time=value))
-    print("=====================================================================")
-    for key, value in average_teleop_ownership_eventleaderboard(EVENT, "switch"):
-        print("{team} : {time}s".format(team=key,time=value))
+if __name__ == "__main__":   
+    
+    print("SERT")
+    print("Scale:  ",average_teleop_ownership("frc2521", "scale", True))
+    print("Switch:  ",average_teleop_ownership("frc2521", "switch", True))
+    print("==============================================")
+    print(TEAM)
+    print("Scale:  ",average_teleop_ownership(TEAM, "scale", True))
+    print("Switch:  ",average_teleop_ownership(TEAM, "switch", True))
+
+
+
+
+    # for key, value in average_teleop_ownership_eventleaderboard(EVENT, "scale"):
+    #     print("{team} : {time}s".format(team=key,time=value))
+    # print("=====================================================================")
+    # for key, value in average_teleop_ownership_eventleaderboard(EVENT, "switch"):
+    #     print("{team} : {time}s".format(team=key,time=value))
